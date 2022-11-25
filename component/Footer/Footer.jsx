6ad15cdf973b27290b20698b06d85e206dbdc523
sakata007 <be_best72@yahoo.co.jp>
2022-11-25 23:14:41 +0900

    コピーらいと設定

--- component/Footer/Footer.jsx
+++ component/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import styles from "./Footer.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <small className={styles.small}>&:copy 2022 lettuce of Arai</small>
+      <small className={styles.small}>&copy; 2022 lettuce of Arai</small>
     </footer>
   );
 };
