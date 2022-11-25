352a8add9b33a68ae68402b32210032346256398
sakata007 <be_best72@yahoo.co.jp>
2022-11-25 13:33:49 +0900

    first commit

---
+++ component/layout.jsx
@@ -0,0 +1,11 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Head>{/* <link rel="stylesheet" href="" /> */}</Head>
+      <main>{children}</main>
+    </div>
+  );
+}
