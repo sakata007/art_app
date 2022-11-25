ed1f013401d1eb84b4835485f008466e6c238650
sakata007 <be_best72@yahoo.co.jp>
2022-11-25 18:30:15 +0900

    ハンバーガー概ね実装完了

---
+++ component/Hamburger/Hamburger.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import styles from "./Hamburger.module.scss";
+
+const Hamburger = () => {
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const toggleMenu = () => {
+    setIsOpenMenu(!isOpenMenu);
+    // alert("とぐる");
+  };
+  return (
+    <div className={styles.parent}>
+      <button
+        className={styles.button}
+        onClick={() => {
+          toggleMenu();
+        }}
+      >
+        <div
+          className={`${styles.button__inner} ${
+            isOpenMenu ? styles.closeButton : ""
+          }`}
+        >
+          <span className={styles.button__inner__line}></span>
+          <span className={styles.button__inner__line}></span>
+          <span className={styles.button__inner__line}></span>
+        </div>
+      </button>
+
+      <nav
+        className={`${styles.hamburger__nav} ${
+          isOpenMenu ? styles.hamburger__opened : ""
+        }`}
+      >
+        <ul className={styles.opened__ul}>
+          <li className={styles.opened__li}>top</li>
+          <li className={styles.opened__li}>works</li>
+          <li className={styles.opened__li}>store</li>
+          <li className={styles.opened__li}>profile</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Hamburger;
--- component/Main/TopMain.jsx
+++ component/Main/TopMain.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styles from "./TopMain.module.scss";
 
 const TopMain = () => {
-  return <div></div>;
+  return (
+    <div className={styles.topMain}>
+      <h1 className={styles.h1}>メインです</h1>
+    </div>
+  );
 };
 
 export default TopMain;
