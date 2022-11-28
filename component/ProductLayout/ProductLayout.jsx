3ca61d4e71421fa59e671659eb50e170d4237fd9
sakata007 <be_best72@yahoo.co.jp>
2022-11-28 22:55:22 +0900

    product実装中

--- component/ProductLayout/ProductLayout.jsx
+++ component/ProductLayout/ProductLayout.jsx
@@ -4,24 +4,29 @@ import styles from "./ProductLayout.module.scss";
 
 const ProductLayout = () => {
   return (
-    <div className={styles.productLayout}>
-      <div className={styles.productLayout__imagesArea}>
-        <DetialImageContainer />
+    <div>
+      <div className={styles.productLayout}>
+        <div className={styles.productLayout__imagesArea}>
+          <DetialImageContainer />
+        </div>
+        <div className={styles.productLayout__textArea}>
+          <section className={styles.product}>
+            <h1 className={styles.product__title}>
+              clown
+              <br />
+              sticker
+            </h1>
+            <div className={styles.product__summary}>
+              <p className={styles.product__size}>size W86mm × H29mm</p>
+              <p className={styles.product__price}>
+                <span className={styles.product__price__border}>1P ¥500</span>
+              </p>
+            </div>
+          </section>
+        </div>
       </div>
-      <div className={styles.productLayout__textArea}>
-        <section className={styles.product}>
-          <h1 className={styles.product__title}>
-            clown
-            <br />
-            sticker
-          </h1>
-          <div className={styles.product__summary}>
-            <p className={styles.product__size}>size W86mm × H29mm</p>
-            <p className={styles.product__price}>
-              <span className={styles.product__price__border}>1P ¥500</span>
-            </p>
-          </div>
-        </section>
+      <div className={styles.contact}>
+        ご注文はInstagramのDMからお問い合わせください。
       </div>
     </div>
   );
